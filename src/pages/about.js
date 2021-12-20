7578808bd8139051bb1bdd49bb54ccb18cb41113
Amber Joy <amber.joy.dev@gmail.com>
2021-12-19 23:57:26 -0600

    move img to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,16 @@
-import * as React from 'react'
-import Layout from '../components/layout'
+import * as React from "react";
+import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
+      <StaticImage alt="Brody and me" src="../images/brodyandme.jpg" />
     </Layout>
-  )
-}
+  );
+};
 
-export default AboutPage
+export default AboutPage;
